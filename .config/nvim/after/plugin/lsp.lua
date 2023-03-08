5dac89f3f5c2e468ba0a1a76dc7d18dd94362fe7
Leo Spratt <enchant97@users.noreply.github.com>
2023-03-08 20:37:39 +0000

    add more languages to install for lsp+syntax

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,18 @@
 local lsp = require('lsp-zero').preset("recommended")
 
 lsp.ensure_installed({
-	'rust_analyzer',
+    -- LSP's
+    'rust_analyzer',
+    'gopls',
+    'pyright',
+    'tsserver',
+    'lua_ls',
+    'html',
+    'cssls',
+    'jsonls',
+    'yamlls',
+    'dockerls',
+    'texlab',
 })
 
 lsp.setup()
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,14 +1,10 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "help", "lua", "c", "javascript", "typescript", "html", "rust", "go", "python" },
-
-  sync_install = false,
-
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "help", "lua", "c", "javascript", "javascript", "typescript", "html", "css", "rust", "go", "python", "yaml", "json", "markdown" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
 }
-
