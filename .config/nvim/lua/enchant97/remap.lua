95ef5d51da7de97748cc86d130640be672fdbb6e
Leo Spratt <enchant97@users.noreply.github.com>
2023-03-24 11:22:05 +0000

    disable nvim c-z suspend

--- .config/nvim/lua/enchant97/remap.lua
+++ .config/nvim/lua/enchant97/remap.lua
@@ -1,4 +1,6 @@
 -- what is the leader key
 vim.g.mapleader = " "
+-- disable suspend process
+vim.keymap.set("n", "<c-z>", "<nop>")
 -- open directory explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Open Directory Explorer' })
